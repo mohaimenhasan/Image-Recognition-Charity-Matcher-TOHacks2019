2853be0be653dc68734c9bf675f9f6afbf07fcaa
Benjamin Labrecque <benjamin.labrecque@mail.mcgill.ca>
2019-06-22 19:55:06 -0400

    added maps.js

---
+++ frontend/src/maps.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
+
+
+class Maps extends Component{
+
+    render() {
+        return (
+            <Map
+                google={this.props.google}
+                zoom={8}
+                style={mapStyles}
+                initialCenter={{ lat: 43.646202, lng: -79.385963}}
+            >
+            <Marker position={{ lat: 43.646202, lng: -79.385963}} />
+        </Map>
+        );
+    }
+}
+
+const mapStyles = {
+    width: '100%',
+    height: '100%',
+  };
+  
+export default GoogleApiWrapper({
+    apiKey: 'AIzaSyClGuqvS5S9nLZiy6oxMgEa8n1UfYCtOBw'
+  })(Maps);
