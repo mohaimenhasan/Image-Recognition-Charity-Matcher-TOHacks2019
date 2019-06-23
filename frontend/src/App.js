f1a37f0ed03162963859c2301c900662d38f0c74
jhunnie <jenniej.hu@mail.utoronto.ca>
2019-06-22 20:05:33 -0400

    added boostrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
@@ -20,6 +21,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
