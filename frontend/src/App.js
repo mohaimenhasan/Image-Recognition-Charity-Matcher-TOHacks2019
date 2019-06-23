158f958c6ac955d2398895dec7110860db6afa66
jhunnie <jenniej.hu@mail.utoronto.ca>
2019-06-23 00:09:18 -0400

    added map footer, photo upload works for users

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react';
 //import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 import User from './components/user'
 import Charity from './components/charity'
 import Home from './components/home'
+import Maps from './maps'
+
 
 class App extends Component {
   constructor(props) {
@@ -26,15 +28,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <div className="App">
-          {this.state.currentScreen}
-        </div>
+
         <Router>
           <div className="App">
             <header className="App-header">
-              <a href="/" class="btn btn-info" role="button" id="nav-button">Home</a>
-            </header>
+              <a href="/" className="btn btn-info" role="button" id="nav-button">Home</a>
 
+            </header>
+            <div className="map">
+              {this.state.currentScreen}
+            </div>
           </div>
           <Switch>
             <Route exact path='/' component={Home} />
--- frontend/src/components/user.js
+++ frontend/src/components/user.js
@@ -5,12 +5,29 @@ class User extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            items: []
+            photo: {}
         };
+
+        this.loadFile = this.loadFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit() {
-        
+    handleSubmit(event) {
+        console.log('photo received: ', event.target.files[0].name);
+        this.setState({
+            photo: event.target.files[0]
+        });
+    }
+
+    loadFile(event){
+        // var image = document.getElementById('output');
+        // image.src = URL.createObjectURL(event.target.files[0]);
+        //handleSubmit(event);
+        console.log('photo received: ', event.target.files[0].name);
+        this.setState({
+            photo: event.target.files[0]
+        });    
+        //event.target.files[0] accesses first file that was uploaded
     }
 
     render() {
@@ -19,8 +36,9 @@ class User extends Component {
                 <h2> Welcome to Add Item </h2>
                 <form onSubmit={this.handleSubmit}>
                     Upload A Photo:
-                    <input id="uploadImage" type="file" name="photo"/>
-                    <input type='submit' value='Upload Photos' className='btn btn-primary' />
+                    <input type="file" accept="image/*" name="image" id="file" onChange={this.loadFile} style={{ "display": "none" }}></input>
+                    <br/><label className='btn btn-primary' for="file" style={{ "cursor": "pointer" }}>Choose File</label>
+                    {/* <p><img id="output" width="200" alt='upload' /></p> */}
                 </form>
             </div>
         );
